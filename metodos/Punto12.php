ae5b35c22816d0a8f59a6fe6210ec493ffc68943
lvillamizarmurillo <lvillamizarmurillo@gmail.com>
2023-06-06 11:13:52 -0500

    feat: :sparkles: Punto 13
    Revertir el orden de los planetas en un sistema solar

--- metodos/Punto12.php
+++
@@ -1,8 +0,0 @@
-<?php
-    session_start();
-    echo "Todos los satelites de cada planeta: <br>";
-    print_r($_SESSION['s1']);
-    echo <<<HTML
-        <br><a href="index.php">Volver</a>
-    HTML;
-?>
---
+++ metodos/Punto13.php
@@ -0,0 +1,7 @@
+<?php
+    $sistemaSolar = array("Neptuno","Urano","Saturno","Jupiter","Marte","Tierra","Venus","Mercurio");
+    print_r(array_reverse($sistemaSolar));
+    echo <<<HTML
+        <br><a href="index.html">Volver</a>
+    HTML;
+?>
